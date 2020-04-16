d050c66e1c17f2a997acd6635f7735e866f12d27
hchau98 <chauhenry02@gmail.com>
2020-04-16 18:34:55 -0400

    Second Commit

--- src/GreatestNum.java
+++ src/GreatestNum.java
@@ -13,12 +13,11 @@ public class GreatestNum {
         int num3 = in.nextInt();
 
 
-        if (num1 > num2)
-            if (num1 > num3)
-                System.out.println("The greatest: " + num1);
+        if (num1 > num2&&num1 > num3)
+            System.out.println("The greatest: " + num1);
+            else if(num2 > num3)
+                System.out.println("The greatest: " + num2);
             else
                 System.out.println("The greatest: " + num3);
-        else
-            System.out.println("The greatest: " + num2);
     }
 }
